6f1448d11fa05d794d2781373d7fdd9dcee5e0a2
Dusan Petrovic <dusanp93@icloud.com>
2020-01-01 23:11:06 +0100

    finished simple to do list in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import classes from "./App.module.css";
+import withClass from "./hoc/withClass";
+import Aux from "./hoc/Aux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: "",
+      textList: []
+    };
+  }
+
+  handleChangeText = event => {
+    this.setState({
+      inputText: event.target.value
+    });
+  };
+
+  handleButtonClick = input => {
+    let listArray = this.state.textList;
+
+    listArray.push(input);
+    this.setState({
+      textList: listArray,
+      inputText: ""
+    });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <div>
+          <input
+            value={this.state.inputText}
+            type="text"
+            onChange={event => this.handleChangeText(event)}
+          />
+          <button onClick={() => this.handleButtonClick(this.state.inputText)}>
+            Dodaj
+          </button>
+
+          <ul>
+            {this.state.textList.map(text => {
+              return <li>{text}</li>;
+            })}
+          </ul>
+        </div>
+      </Aux>
+    );
+  }
 }
 
-export default App;
+export default withClass(App, classes.App);
